2db40bb6f950b4497d5c54f64e5d2ffecf59d004
DESKTOP-BP26QNU\ADMIN <phandduong98@gmail.com>
2023-07-27 17:03:57 +0700

    MongoClient connect

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
-import cors from 'cors';
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
 
-import router from './router';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import router from "./router";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  credentials: true,
-}));
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
 app.use(compression());
 app.use(cookieParser());
@@ -26,10 +28,23 @@ server.listen(process.env.PORT, () => {
   console.log(`Server running on http://localhost:${process.env.PORT}/`);
 });
 
-const mongoUrl = process.env.MONGO_URL; 
+const mongoUrl = process.env.MONGO_URL;
 
-mongoose.Promise = Promise;
-mongoose.connect(mongoUrl!);
-mongoose.connection.on('error', (error: Error) => console.log(error));
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(mongoUrl!);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-app.use('/', router());
+app.use("/", router());
+
+//Connect to the database before listening
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log("listening for requests");
+  });
+});
